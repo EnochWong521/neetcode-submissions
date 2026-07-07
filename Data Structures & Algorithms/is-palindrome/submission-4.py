ada43e5566aa7800837145a0a01cc71d805ff795
Enoch Wong <98492178+EnochWong521@users.noreply.github.com>
2026-07-07 00:37:00 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,26 @@
+# abba 2
+# ababa int(2.5) = 2
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        idx1 = 0
+        idx2 = len(s) - 1
+
+        while(idx1 < idx2):
+            # get left ptr to alphanumeric char
+            while (idx1 < idx2 and not s[idx1].isalnum()):
+                idx1 += 1
+            # get right ptr to alphanumeric char
+            while (idx1 < idx2 and not s[idx2].isalnum()):
+                idx2 -= 1
+            # compare
+            # mismatch
+            if (s[idx1].lower() != s[idx2].lower()):
+                return False
+            # match
+            else:
+                idx1 += 1
+                idx2 -= 1
+        return True
+
+            
